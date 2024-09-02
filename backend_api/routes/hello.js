254262e56236265cf8bc3b3fe4ed0964d4ddd8b2
asindrawancode <agussrindrawan@gmail.com>
2024-09-03 00:15:12 +0700

    Introduction To Node js

---
+++ backend_api/routes/hello.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const helloRoute = express.Router();
+
+helloRoute.get('/hello', (req, res)=>{
+    res.send('Hello World');
+});
+
+module.exports = helloRoute;
