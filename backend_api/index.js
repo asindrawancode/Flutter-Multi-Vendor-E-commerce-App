b2cda8845a81ea3bbc77661594c4713eb18df19f
asindrawancode <agussrindrawan@gmail.com>
2024-09-03 23:51:35 +0700

    Integrated MongoDB

--- backend_api/index.js
+++ backend_api/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const helloRoute = require('./routes/hello');
+const mongoose  = require('mongoose');
+
 const PORT = 3000;
 
 const app = express();
 
+const DB = "mongodb+srv://agussriindrawan:!!Password12345!!@cluster0.ajfgo.mongodb.net/";
+
 app.use(helloRoute);
+
+mongoose.connect(DB).then(()=>{
+    console.log('mongodb connected');
+});
 
 app.listen(PORT,"0.0.0.0", function(){
     console.log(`server is running on port ${PORT}`)
